741ba4e2420315e9a5127d67b6ab784722349f0e
shahad-mahmud <shahad9381@gmail.com>
2020-04-28 01:27:27 +0600

    starting project

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'layouts/home.dart';
+import 'layouts/choose_location.dart';
+import 'layouts/loading.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
+
